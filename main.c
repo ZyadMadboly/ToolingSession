8eb56a9ad1fadce862d3269b9854879ed30bf2d4
ZyadMadboly <zeiadmadboly@gmail.com>
2021-01-02 15:33:19 +0200

    I have Modifed this

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("Please Enter a number\n");
+    scanf("%d",& x);
+    printf("The Number is %d",x);
+    return 0;
+}
